aec92e1dfc162c5b892f266073720ffcf01870f7
Noe Solis <noesolism@hotmail.com>
2019-04-28 13:44:58 -0500

    Actualización 4

---
+++ app/src/main/java/com/example/borrar/Monica.java
@@ -0,0 +1,13 @@
+package com.example.borrar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Monica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monica);
+    }
+}
